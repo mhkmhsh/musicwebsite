28b97f7de55dd88c9790c3de82770947028f55c2
mhkmhsh <mahekmahesh12@gmail.com>
2025-01-18 11:31:22 -0500

    finished the layout of the first page, must get the log in or sign up bar to pop up

--- genre.java
+++ genre.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class genre {
 
 public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter you Artist name : ")
+String name = scanner.nextLine();
+System.out.print("Artrist genres ")
 
-    
+System.out.println("")
+
 }
 }
---
+++ index.js
@@ -0,0 +1,28 @@
+(function($){
+    // variables
+    elementWidth = $('ul').width(),
+    containerWidth = $('nav').width(),
+    difference = elementWidth-containerWidth,
+    finalWidth = difference * 1.5,
+    element = $('ul');
+  
+    $('li').on('click', function(){
+      $('li').removeClass('active');
+      $(this).addClass('active');
+    });
+    
+  })(jQuery);
+  
+  $(document).ready(function() {
+    $('.event-heading').on('click', function() {
+        $(this).next('.event-details').slideToggle();
+    });
+  });
+  
+  $(document).ready(function() {
+    $('.trunk-image').on('mouseenter', function() {
+      $(this).find('.description').fadeIn();
+    }).on('mouseleave', function() {
+      $(this).find('.description').fadeOut();
+    });
+  });
